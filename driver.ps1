0f61b1dbbdf20ffd7120fc6e5c9fb34872650009
kristie437 <107785489+kristie437@users.noreply.github.com>
2022-10-28 15:59:25 -0400

    Update driver.ps1

--- driver.ps1
+++ driver.ps1
@@ -148,6 +148,7 @@ function ChangeLocalPasswords ($ServersList) {
   $ServersList | %{
     Try {
 		& $cd\PsExec.exe \\$_ -nobanner -accepteula powershell -command "Add-Type -AssemblyName System.Web;`$c = ','; `$h=`$(hostname); Get-LocalUser | ?{`$_.Name -ne 'Administrator'} | %{`$pass=[System.Web.Security.Membership]::GeneratePassword(20,2); Set-LocalUser -Name `$_.Name -Password (ConvertTo-SecureString -AsPlainText `$pass -Force); Write-Host `$h\`$_`$c`$pass; `$pass = `$Null}" >> C:\incred.csv
+		#& $cd\PsExec.exe \\$_ -nobanner -accepteula powershell -command "Add-Type -AssemblyName System.Web;`$c = ','; `$h=`$(hostname); Get-LocalUser | ?{`$_.Name -ne 'Administrator'} | %{`$pass=[System.Web.Security.Membership]::GeneratePassword(20,2); Set-LocalUser -Name `$_.Name -Password (ConvertTo-SecureString -AsPlainText `$pass -Force); Write-Host `$h\`$_`$c`$pass; `$pass = `$Null}" >> C:\incred.csv
 	}
     Catch {
     	  Write-Output "Could not access " $_
